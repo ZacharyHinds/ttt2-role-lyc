ef702d191d3560edbeb4b782ff8549aafa772955
Tim Goll <tim@timgoll.de>
2020-08-24 22:38:24 +0200

    added german translation

---
+++ lua/terrortown/lang/deutsch/lycanthrope.lua
@@ -0,0 +1,17 @@
+L = LANG.GetLanguageTableReference("deutsch")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[LYCANTHROPE.name] = "Werwolf"
+L["info_popup_" .. LYCANTHROPE.name] = [[Du bist ein Werwolf! Arbeite mit den Unschuldigen um zu gewinnen!]]
+L["body_found_" .. LYCANTHROPE.abbr] = "Er war ein Werwolf!"
+L["search_role_" .. LYCANTHROPE.abbr] = "Diese Person war ein Werwolf!"
+L["target_" .. LYCANTHROPE.name] = "Werwolf"
+L["ttt2_desc_" .. LYCANTHROPE.name] = [[Der Werwolf ist ein Unschuldiger, der mit diesen arbeitet um zu gewinnen.
+Wenn er der letzte lebende ist, wird er schneller und stärker!]]
+
+-- EPOP LANGUAGE STRINGS
+
+L["ttt2_lyc_transform_title"] = "Es ist Vollmond!"
+L["ttt2_lyc_transform_body"] = "Der Werwolf wurde entfesselt!"
+L["ttt2_lyc_self_transform_body"] = "Du bist der Werwolf! Deine Kräfte wurden entfesselt!"
+L["ttt2_lyc_multi"] = "Schaden x{multi}"
