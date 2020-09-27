6d6fccc717d3ba6f071aa1d6dc41b5d3c5ce65b3
Blueberryy <igory.ygr200@gmail.com>
2020-09-27 19:26:03 +0500

    Create lycanthrope.lua

---
+++ lua/terrortown/lang/russian/lycanthrope.lua
@@ -0,0 +1,17 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[LYCANTHROPE.name] = "Ликантроп"
+L["info_popup_" .. LYCANTHROPE.name] = [[Вы ликантроп! Работайте с невиновными, чтобы победить!]]
+L["body_found_" .. LYCANTHROPE.abbr] = "Он был ликантропом!"
+L["search_role_" .. LYCANTHROPE.abbr] = "Этот человек был ликантропом!"
+L["target_" .. LYCANTHROPE.name] = "Ликантроп"
+L["ttt2_desc_" .. LYCANTHROPE.name] = [[Ликантроп - это невиновная роль, и он работает с невиновными, чтобы победить.
+Если он останется последним, они станет быстрее и сильнее!]]
+
+-- EPOP LANGUAGE STRINGS
+
+L["ttt2_lyc_transform_title"] = "Восход полной луны!"
+L["ttt2_lyc_transform_body"] = "Ликантроп был освобождён!"
+L["ttt2_lyc_self_transform_body"] = "Вы ликантроп! Ваши силы были освобождены!"
+L["ttt2_lyc_multi"] = "Урон x{multi}"
